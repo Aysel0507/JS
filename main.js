37b06075252f5611e7b5427267736c7b69d4ca20
TSpg1012 <bd8g57ev2@code.edu.az>
2024-03-27 20:00:55 +0400

    first

---
+++ main.js
@@ -0,0 +1,50 @@
+// let second=window.prompt("enter second");
+// console.log(parseInt(second/60)+"deqiqe"+(second%60)+"saniye");
+
+// let selsi=window.prompt("enter temp");
+// console.log(parseFloat(x=5/9*(selsi-32)));
+
+// let a,b,c,d,e
+// a=+window.prompt("enter score");
+// b=+window.prompt("enter score");
+// c=+window.prompt("enter score");
+// d=+window.prompt("enter score");
+// e=+window.prompt("enter score");
+// console.log(((a+b+c+d+e)/5));
+
+// let year=window.prompt("enter year");
+// console.log(parseInt(2024-year));
+
+// let a,b;
+// a=window.prompt("enter year");
+// b=window.prompt("enter year");
+// console.log(parseInt((a*b)/100));
+
+// let a,b;
+// a=window.prompt("enter year");
+// b=window.prompt("enter year");
+// console.log(parseInt((a*b)/2));
+
+// let a,b,c;
+// a=window.prompt("enter year");
+// b=window.prompt("enter year");
+// c=window.prompt("enter year");
+// console.log(parseInt(a*b/100/c));
+
+// let firstName,lastName;
+// firstName=window.prompt("enter year");
+// lastName=window.prompt("enter year");
+// console.log(parseInt(hello));
+
+// let a=window.prompt("enter year");
+// let p=4*a;
+// let s=a**2;
+// console.log(p,s);
+
+
+let a,b;
+a=window.prompt("enter year");
+b=window.prompt("enter year");
+console.log(((a+b)**3) (a**3+b**3));
+
+
