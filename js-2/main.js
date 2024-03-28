ebbec911a0cb6112c351b28cf6b5b3e3976f30b6
TSpg1012 <bd8g57ev2@code.edu.az>
2024-03-28 19:43:38 +0400

    28-03

---
+++ js-2/main.js
@@ -0,0 +1,254 @@
+// a=window.prompt("enter age:");
+// if(a>0 && a<125){
+//     if(a>=65){
+//         console.log("catib")
+//     }
+//     else{
+//         console.log("catmiyib")
+//     }
+// }
+// else{
+//     console.log("invalid")
+// }
+
+// 222222222222222222222222
+// a=window.prompt("enter number:");
+// if (a%2==0){
+//     console.log("cut");
+// }
+// else if(a==0){
+//     console.log("ne cut, ne tek");
+// }
+// else{
+//     console.log("tek");
+// }
+
+// 33333333333333333333
+
+// let ay=window.prompt("enter:")
+// switch (ay) {
+//     case "yanvar":
+//     case "dekabr":
+//     case "fevral":
+//         console.log("qis ayidi");
+//         break;
+
+//     case "mart":
+//     case "aprel":
+//     case "may":
+//         console.log("yaz ayidi");
+//         break;
+
+//     case "iyun":
+//     case "iyul":
+//     case "avqust":
+//         console.log("yay ayidi");
+//         break;
+
+//     case "sentyabr":
+//     case "oktyabr":
+//     case "noyabr":
+//         console.log("payiz ayidi");
+//         break;
+
+// }
+
+// let a=window.prompt("enter:");
+// if(a%3==0 && a%5==0){
+//     console.log("bolunur")
+// }
+// else{
+//     console.log("bolunmur")
+// }
+
+// let a=window.prompt("enter number:");
+// if(a>0 && a%2==0){
+//     console.log("bolundu")
+// }
+// else{
+//     console.log(a%2)
+// }
+
+// let a=window.prompt("enter number:");
+// let b=window.prompt("enter number:");
+
+// if(a*b>0){
+//     console.log("musbet")
+// }
+// else{
+//     console.log("menfi")
+// }
+
+// 888888888888888888888888
+
+// let a = window.prompt("enter number:");
+// let b = window.prompt("enter number:");
+// let c = window.prompt("enter number:");
+
+// if(a>b && a>c){
+//     console.log("ilk eded daha boyukdur")
+// }
+// else if(b>a && b>c){
+//     console.log("ikinci eded daha boyukdur")
+
+// }
+// else{
+//     console.log("3 eded daha boyukdur")
+// }
+
+// 77777777777777777777777777777777777777777777777777777777777777
+// let n=window.prompt("enter:");
+
+// if(n>=1 && n<100){
+//     console.log("daxildedir")
+// }
+// else{
+//     console.log("daxilinde deil")
+// }
+
+// 10101010101010101010101010101010101010
+
+// let b=window.prompt("enter bal:");
+
+// if(b>0 && b<100){
+//     if(b>90){
+//         console.log("A");
+// }
+//     else if(b>=80 && b<=89){
+//         console.log("B");
+// }
+
+//     else if(b>=70 && b<=79){
+//         console.log("C");
+// }
+
+//     else if(b=59 && b<=69){
+//         console.log("D");
+// }
+
+//     else{
+//         console.log("F");
+// }
+
+// }
+
+// let a=Number(window.prompt("enter:"))
+// let b=Number(window.prompt("enter:"))
+// let c=window.prompt("enter:")
+
+// switch (c) {
+//     case "+":
+//         console.log(a+b);
+//         break;
+//     case "-":
+//         console.log(a-b);
+//         break;
+//     case "*":
+//         console.log(a*b);
+//         break;
+//     case "/":
+//         console.log(a/b);
+//         break;
+// }
+
+// for (let i = 0; i < 100; i++){
+//     if (i%2==0){
+//         console.log(i)
+//     }
+// }
+
+// let n=window.prompt("enter")
+// for (let i = 0; i < 100; i++){
+//     if(n%i==0){
+//         console.log(i)
+//     }
+// }
+
+// let a=window.prompt("enter")
+// let b=window.prompt("enter")
+// let c=window.prompt("enter")
+
+// if(a**2+b**2==c**2 | c**2+b**2==a**2 | a**2+c**2==b**2 ){
+//     console.log("duzbucaqli ucbucaqdir")
+// }
+
+// else{
+//     console.log("duzbuaqli deil")
+// }
+
+// let a = window.prompt("enter");
+// let b;
+// let c;
+
+// b=parseInt(a/100%10)
+// c=parseInt(a/10%10)
+// d=parseInt(a%10)
+
+// console.log(b,c,d)
+
+// let a = window.prompt("enter");
+// let b = window.prompt("enter");
+// let c = window.prompt("enter");
+
+// if(a==b && a==c && b==c){
+//     console.log("beraber")
+// }
+// else{
+//     console.log("beraber deil")
+// }
+
+// let a = window.prompt("enter");
+// let b = window.prompt("enter");
+// let c = window.prompt("enter");
+
+// if(a==b | a==c | b==c ){
+//     console.log("beraberyanli")
+// }
+// else{
+//     console.log("beraberyanli deil")
+// }
+
+// let a = window.prompt("enter");
+// let b = window.prompt("enter");
+// let c = window.prompt("enter");
+
+// if(a+b>c && Math.abs(a-b)<c | a+c>b && Math.abs(a-c)<b | b+c>a && Math.abs(b-c)<a){
+//     console.log("ucbucaqdir")
+
+// }
+// else{
+//     console.log("ucbucaq deil")
+// }
+
+// let month=window.prompt("enter:")
+
+// switch (month) {
+//     case "january":
+//     case "March":
+//     case "May":
+//     case "July":
+//     case "August":
+//     case "October":
+//     case "December":
+//         console.log(' has 31 days');
+//         break;
+//     case "February":
+//         console.log(' has 28 days');
+//         break;
+//     case "april":
+//     case "june":
+//     case "september":
+//     case "November":
+//         console.log(' has 30 days');
+//         break;
+// }
+
+let a = window.prompt("enter");
+let b;
+let c;
+
+b = parseInt((a / 100) % 10);
+c = parseInt((a / 10) % 10);
+d = parseInt(a % 10);
+// adi qayda da tap
+console.log();
