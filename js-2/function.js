22a40147224a79af5866764d68d881e6ea9d0093
TSpg1012 <bd8g57ev2@code.edu.az>
2024-03-29 19:54:19 +0400

    added

---
+++ js-2/function.js
@@ -0,0 +1,207 @@
+// function printFulname(firstName,lastName){
+//     return `I am ${firstName} ${lastName} `
+
+// }
+// console.log(printFulname("Aysel","Qanbarova"))
+
+
+
+
+
+
+
+// 222222222222222222222222222222222222222222222222222222222222222222
+
+// function square(number){
+//     return number**2;
+// }
+
+// console.log(square(5))
+
+
+
+
+
+
+
+// 33333333333333333333333333333333333333333333
+
+// function func1(num1,num2){
+//     return sum=num1+num2;
+// }
+// console.log(func1(5,3));
+
+// function func2(num1,num2){
+//     return num1*num2;
+// }
+// console.log(func2(5,3));
+
+// function func3(num1,num2){
+//     return subtruct=num1-num2;
+// }
+// console.log(func3(5,3));
+
+// function func4(num1,num2){
+//     return divide=num1/num2;
+// }
+// console.log(func4(5,3));
+
+
+
+
+
+
+
+
+// 444444444444444444444444444444
+
+// function calc(num1, num2, operator) {
+//     if(operator=="+"){
+//         return sum=num1+num2;
+//     }
+//     else if(operator=="*"){
+//         return sum=num1*num2;
+//     }
+//     else if(operator=="/"){
+//         return sum=num1/num2;
+//     }
+//     else if(operator=="-"){
+//         return sum=num1-num2;
+//     }
+
+// }
+
+// console.log(calc(2,3,"+"))
+
+
+
+
+
+
+// 55555555555555555555555555555555555555
+
+// const getSum = (array) => {
+//     return array[0]+array[array.length-1];
+// };
+
+// console.log(getSum([1, 2, 3])); // 4
+// console.log(getSum([80, 5, 58, 17, 100])); // 180
+// console.log(getSum([15, -500, 0, 50])); //65
+
+
+
+
+
+
+// 666666666666666666666666666666666666666666666666
+
+
+
+
+
+
+
+
+
+
+
+// 77777777777777777777777777777777777777
+
+// const getSum = (array) => {
+//     let max=array[0]
+//     for( let i=0; i<array.length; i++){
+//         if(array[i]>max){
+//         max=array[i];
+//         }
+
+// }
+// return max;
+// };
+// console.log(getSum([1, 2, 3]));
+
+
+
+
+
+
+
+// 88888888888888888888888888888
+
+// const employees= [
+//     { name: "Jamil", salary: 50000, department: "IT" },
+//     { name: "Jale", salary: 60000, department: "HR" },
+//     { name: "Bayram", salary: 55000, department: "IT" },
+//     { name: "Sahil", salary: 75000, department: "HR" },
+//     { name: "Maryam", salary: 65000, department: "IT" },
+//     { name: "Elnara", salary: 80000, department: "HR" },
+//     { name: "Davud", salary: 70000, department: "IT" },
+//   ];
+
+// function employee(item){
+//     return item.salary > 60000;
+// }
+
+// console.log(employees.filter(employee))
+
+
+
+
+
+
+
+// 9999999999999999999999999999999999
+
+// function findElement(arr, number) {
+//   let bool = false;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] == number) {
+//       bool = true;
+//       break;
+//     }
+//   }
+
+//   if (bool) {
+//     return "bele bir element var";
+//   }
+//   else{
+//     return "bele bir element yoxdur"
+//   }
+// }
+
+// console.log(findElement([33, 28, 38, 45, 17, 38], 38));
+
+
+
+
+
+
+// 11)
+
+// function concatArray(arr1,arr2){
+//     return arr1.concat(arr2);
+// }
+// console.log(concatArray([1,2,3,4,5],[6,7,8,9]))
+
+
+
+
+
+
+// 12)
+// function array_filled(arr, num) {
+//   let result = [];
+//   for (let i = 0; i < num; i++) {
+//     result += arr;
+//   }
+//   return result;
+// }
+
+// console.log(array_filled(0,6));
+
+
+
+
+
+
+// 13)
+
